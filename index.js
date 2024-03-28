ef718c380b4d85d29e34e507b1acae112ac011bf
unknown <najmiddinnasriddinov5@gmail.com>
2024-03-28 20:49:10 +0500

    JavaScript masalalar ishlandi

---
+++ index.js
@@ -0,0 +1,48 @@
+    // 1-MASALA
+// let amount = prompt('Pulingni kirit Alisher');
+// let fare = 500 * 11000.34;
+// let hotel = 250 * 11000.34;
+// let another = 120 * 12354.03;
+
+// if (amount >= (fare + hotel + another)) {
+//     alert(`Oq yo'l Alisher ;)`)
+// } else {
+//     alert(`Alisher ozgina sabir qilishing kerak bo'lar ekan :(`);
+// };
+
+
+
+    // 2-MASALA
+let weekDays = prompt();
+
+if (weekDays > 7) {
+    alert(`Bunday hafta kuni mavjud emas`)
+}
+
+if (weekDays == 1) {
+    alert(`Dushanba`)
+} 
+
+if (weekDays == 2) {
+    alert(`Seshanba`)
+}
+
+if (weekDays == 3) {
+    alert(`Chorhsanba`)
+}
+
+if (weekDays == 4) {
+    alert(`Payshanba`)
+}
+
+if (weekDays == 5) {
+    alert(`Juma`)
+}
+
+if (weekDays == 6) {
+    alert(`Shanba`)
+}
+
+if (weekDays == 7) {
+    alert(`Yakshanba`)
+}
